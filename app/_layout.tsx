b58b5dec188c9eba3b16a49aff72ae45e95cf4ba
samuelsenerwa <samuelsenerwa@gmail.com>
2025-03-17 12:41:15 +0300

    chore: initialized modals && listing details

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,9 @@
+import { Ionicons } from '@expo/vector-icons';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { router, Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+import { TouchableOpacity } from 'react-native';
 import 'react-native-reanimated';
 
 export {
@@ -49,6 +51,36 @@ function RootLayoutNav() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="(modals)/login"
+        options={{
+          title: 'Login or Sign up',
+          headerTitleStyle: {
+            fontFamily: 'lex-semi'
+          },
+          presentation: 'modal',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()} >
+              <Ionicons name='close-outline' size={28} />
+            </TouchableOpacity>
+          )
+        }} />
+      <Stack.Screen
+        name="listing/[id]"
+        options={{ headerTitle: '' }}
+      />
+      <Stack.Screen
+        name="(modals)/booking"
+        options={{
+          presentation: 'transparentModal',
+          animation: 'fade',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()} >
+              <Ionicons name='close-outline' size={28} />
+            </TouchableOpacity>
+          )
+        }}
+      />
     </Stack>
   );
 }
