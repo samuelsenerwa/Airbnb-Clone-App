cb55991a793c2c77b10c697d15dce00d570a0d96
samuelsenerwa <samuelsenerwa@gmail.com>
2025-03-19 11:54:12 +0300

    feat: added explore page customHeader

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,25 @@
 import { View, Text } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
+import React, { useState } from 'react'
+import { Link, Stack } from 'expo-router'
+import ExploreHeader from '@/components/ExploreHeader'
+import Listings from '@/components/Listings'
 
 const Page = () => {
+
+    const [category, setCategory] = useState<string>('Tiny homes');
+
+    const onDataChanged = (category: string) => {
+        setCategory(category);
+    };
+
     return (
-        <View>
-            <Link href={"/(modals)/login"}>
-                Login
-            </Link>
-            <Link href={"/(modals)/booking"}>
-                Bookings
-            </Link>
-            <Link href={'/listing/1337'}>Listing Details</Link>
+        <View style={{ flex: 1, marginTop: 80 }}>
+            <Stack.Screen
+                options={{
+                    header: () => <ExploreHeader onCategoryChanged={onDataChanged} />
+                }}
+            />
+            <Listings />
         </View>
     )
 }
