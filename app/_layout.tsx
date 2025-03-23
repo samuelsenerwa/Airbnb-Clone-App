4462fb26bab756e8cf0ede97eab59a7b57ba9694
samuelsenerwa <samuelsenerwa@gmail.com>
2025-03-23 21:19:56 +0300

    feat: added detailed description on a single listing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -102,7 +102,7 @@ function RootLayoutNav() {
         }} />
       <Stack.Screen
         name="listing/[id]"
-        options={{ headerTitle: '' }}
+        options={{ headerTitle: '', headerTransparent: true, }}
       />
       <Stack.Screen
         name="(modals)/booking"
